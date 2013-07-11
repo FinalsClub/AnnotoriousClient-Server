459f2c32fbfad24e206b579deb7ae66c502878b2
Bianca <gando.bia@gmail.com>
2013-07-11 13:53:32 -0700

    mongolabs db

--- public/annotator/config.js
+++ public/annotator/config.js
@@ -17,14 +17,14 @@ $(function() {
 
     // Attach the uri of the current page to all annotations to allow search.
     annotationData: {
-      uri: location.pathname+''
+      uri: location.pathname+''+location.hash
     },
 
     // This will perform a "search" action rather than "read" when the plugin
     // loads. Will request the last 20 annotations for the current url.
     // eg. /store/endpoint/search?limit=20&uri=http://this/document/only
     loadFromSearch: {
-      uri: location.pathname+'',
+      uri: location.pathname+''+location.hash,
       'limit': 100,
     }
   });
