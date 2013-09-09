1aa3e25214e2af3659323fb710e0d784bb9744b9
Walker Flynn <walker.d.flynn@gmail.com>
2013-09-09 12:22:26 -0700

    switching texts working

--- public/backboneApp/views/workView.js
+++ public/backboneApp/views/workView.js
@@ -1,16 +1,53 @@
 var WorkView = Backbone.View.extend({
 
   events: {
-    'click .showAnnotations': 'reload'
+    // 'click .showAnnotations': 'reload'
   },
 
   templateWork: Handlebars.compile(
-    '<button class="button showAnnotations">Show Annotations</button>' +
+    // '<button class="button showAnnotations">Show Annotations</button>' +
     '{{{ html }}}'
   ),
 
-  reload: function() {
-    document.location.reload(true)
+  loadAnnotations: function() {
+    if(!this.$annotations){
+      this.$annotations = $(document.body).annotator();
+
+      this.$annotations.annotator('addPlugin', 'Store', {
+        urls: {
+          // Specify URLs matching our API endpoints
+          create:  '/annotations',
+          read:    '/annotations/:id',
+          update:  '/annotations/:id',
+          destroy: '/annotations/:id',
+          search:  '/annotations/search'
+        },
+
+        // The endpoint of the store on your server.
+        prefix: '',
+
+        // Attach the uri of the current page to all annotations to allow search.
+        // annotationData: {
+        //   uri: location.pathname+''+location.hash
+        // },
+
+        // This will perform a "search" action rather than "read" when the plugin
+        // loads. Will request the last 20 annotations for the current url.
+        // eg. /store/endpoint/search?limit=20&uri=http://this/document/only
+        loadFromSearch: {
+          uri: location.pathname+''+location.hash,
+          'limit': 1000,
+        }
+      });
+      this.annotator = this.$annotations.data('annotator');
+    } else {
+      var options = {
+          uri: location.pathname+''+location.hash,
+          'limit': 1000,
+      };
+      this.annotator.plugins.Store.loadAnnotationsFromSearch(options);
+      console.log(this.annotator);
+    }
   },
 
   render: function(){
@@ -19,7 +56,7 @@ var WorkView = Backbone.View.extend({
       success: function(model, response, options) {
         $('#play-nav a[data-id='+self.model.id+']').addClass('selected').siblings().removeClass('selected');
         self.$el.html(self.templateWork(model.toJSON()));
-        DoAnnotations();
+        self.loadAnnotations();
       },
 
       error: function(err) {
